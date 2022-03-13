1f2675d806c7541e0463839e1745acdcbb11392a
Akshay Trivedi <aku24.7x3@gmail.com>
2022-03-13 16:31:44 -0700

    L + rat

--- src/main.zig
+++ src/main.zig
@@ -45,7 +45,7 @@ pub fn Booleanomial(n: u16) type {
             return ret;
         }
 
-        pub fn not(self: Self) Self {
+        pub fn lnot(self: Self) Self {
             var ret = init_false();
             ret.coeffs[0] = 1 - self.coeffs[0];
             var x: Int = 1;
@@ -58,11 +58,11 @@ pub fn Booleanomial(n: u16) type {
             return ret;
         }
 
-        pub fn @"and"(self: Self, other: Self) Self {
+        pub fn land(self: Self, other: Self) Self {
             return self.mul(other);
         }
 
-        pub fn @"or"(self: Self, other: Self) Self {
+        pub fn lor(self: Self, other: Self) Self {
             var ret = self.mul(other);
             var x: Int = 0;
             while (true) : (x += 1) {
@@ -74,7 +74,7 @@ pub fn Booleanomial(n: u16) type {
             return ret;
         }
 
-        pub fn xor(self: Self, other: Self) Self {
+        pub fn lxor(self: Self, other: Self) Self {
             var ret = self.mul(other);
             var x: Int = 0;
             while (true) : (x += 1) {
@@ -184,16 +184,16 @@ pub fn main() void {
     std.debug.print("b       = {abcd}\n", .{b});
     std.debug.print("c       = {abcd}\n", .{c});
 
-    var @"not a" = a.not();
+    var @"not a" = a.lnot();
     std.debug.print("not a   = {abcd}\n", .{@"not a"});
 
-    var @"a and b" = a.@"and"(b);
+    var @"a and b" = a.land(b);
     std.debug.print("a and b = {abcd}\n", .{@"a and b"});
 
-    var @"a or b" = a.@"or"(b);
+    var @"a or b" = a.lor(b);
     std.debug.print("a or b  = {abcd}\n", .{@"a or b"});
 
-    var @"a xor b" = a.xor(b);
+    var @"a xor b" = a.lxor(b);
     std.debug.print("a xor b = {abcd}\n", .{@"a xor b"});
     std.debug.print("\n", .{});
 
@@ -201,39 +201,39 @@ pub fn main() void {
     std.debug.print("true        = {abcd}\n", .{t});
     std.debug.print("false       = {abcd}\n", .{f});
 
-    var @"not true" = t.not();
+    var @"not true" = t.lnot();
     std.debug.print("not true    = {abcd}\n", .{@"not true"});
-    var @"not false" = f.not();
+    var @"not false" = f.lnot();
     std.debug.print("not false   = {abcd}\n", .{@"not false"});
 
-    var @"a or true" = a.@"or"(t);
+    var @"a or true" = a.lor(t);
     std.debug.print("a or true   = {abcd}\n", .{@"a or true"});
-    var @"a and true" = a.@"and"(t);
+    var @"a and true" = a.land(t);
     std.debug.print("a and true  = {abcd}\n", .{@"a and true"});
-    var @"a xor true" = a.xor(t);
+    var @"a xor true" = a.lxor(t);
     std.debug.print("a xor true  = {abcd}\n", .{@"a xor true"});
-    var @"a or false" = a.@"or"(f);
+    var @"a or false" = a.lor(f);
     std.debug.print("a or false  = {abcd}\n", .{@"a or false"});
-    var @"a and false" = a.@"and"(f);
+    var @"a and false" = a.land(f);
     std.debug.print("a and false = {abcd}\n", .{@"a and false"});
-    var @"a xor false" = a.xor(f);
+    var @"a xor false" = a.lxor(f);
     std.debug.print("a xor false = {abcd}\n", .{@"a xor false"});
 
     std.debug.print("\n", .{});
 
     // advanced
-    var @"c and (a or b)" = c.@"and"(@"a or b");
+    var @"c and (a or b)" = c.land(@"a or b");
     std.debug.print("c and (a or b)      = {abcd}\n", .{@"c and (a or b)"});
 
-    var @"a and b and c and d" = @"a and b".@"and"(c).@"and"(d);
+    var @"a and b and c and d" = @"a and b".land(c).land(d);
     std.debug.print("a and b and c and d = {abcd}\n", .{@"a and b and c and d"});
 
-    var @"a or b or c or d" = @"a or b".@"or"(c).@"or"(d);
+    var @"a or b or c or d" = @"a or b".lor(c).lor(d);
     std.debug.print("a or b or c or d    = {abcd}\n", .{@"a or b or c or d"});
 
-    var @"a xor b xor c xor d" = @"a xor b".xor(c).xor(d);
+    var @"a xor b xor c xor d" = @"a xor b".lxor(c).lxor(d);
     std.debug.print("a xor b xor c xor d = {abcd}\n", .{@"a xor b xor c xor d"});
 
-    var @"(a and b) or (c and (a xor b))" = @"a and b".@"or"(c.@"and"(@"a xor b"));
+    var @"(a and b) or (c and (a xor b))" = @"a and b".lor(c.land(@"a xor b"));
     std.debug.print("(a and b) or (c and (a xor b)) = {abcd}\n", .{@"(a and b) or (c and (a xor b))"});
 }
